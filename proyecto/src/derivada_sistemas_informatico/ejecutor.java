bb8ece8afe822bfdf936f1493b4e0db3df779bef
unknown <jaramilloluis66@gmail.com>
2019-12-08 18:48:31 -0500

    correcion de fallos

--- proyecto/src/derivada_sistemas_informatico/ejecutor.java
+++ proyecto/src/derivada_sistemas_informatico/ejecutor.java
@@ -31,18 +31,21 @@ public class Ejecutor extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox<>();
         jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Derivadas en Informatica");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 180, 440, 40));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 0, 410, 60));
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Escoja ");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 240, -1, -1));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 330, -1, -1));
 
         jComboBox1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Graficar Curva de Produccion", "Hora De Produccion Maxima", "Hora De Produccion Minima", "Graficar Tasa de Produccion" }));
@@ -51,15 +54,21 @@ public class Ejecutor extends javax.swing.JFrame {
                 jComboBox1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 240, -1, -1));
+        getContentPane().add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 330, -1, -1));
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("@lajaramillo5");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 410, 120, -1));
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 410, 120, -1));
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("EFICIENCIA LABORAL DE UNA EMPRESA");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 190, 350, 70));
 
         jLabel3.setIcon(new javax.swing.ImageIcon("C:\\Users\\THE_PUNISHER\\OneDrive\\universidad\\Calculo2019\\calculo_proycto\\proyecto\\utpl-estados-de-flujos-de-efectivo-1-638.jpg")); // NOI18N
         jLabel3.setText("lll");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 630, 480));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 640, 480));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -76,9 +85,13 @@ public class Ejecutor extends javax.swing.JFrame {
             
             dispose();
         }else if (x==2) {
-            
+            HorasProM m= new HorasProM();
+            m.setVisible(true);
+            dispose();
         }else if (x==3) {
-            
+            HorasProMini mini= new HorasProMini();
+            mini.setVisible(true);
+            dispose();
         }else if (x==4) {
             GraficarTasaPro tasa = new GraficarTasaPro();
             tasa.setVisible(true);
@@ -128,5 +141,6 @@ public class Ejecutor extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     // End of variables declaration//GEN-END:variables
 }
